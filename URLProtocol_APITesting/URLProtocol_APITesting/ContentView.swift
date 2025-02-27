2f3e11702aba5808da69d7eb406a29b08ce374f1
oviePeriscope <habibur.rahman@periscopelabs.io>
2025-02-27 07:35:07 +0600

    Add project

---
+++ URLProtocol_APITesting/URLProtocol_APITesting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  URLProtocol_APITesting
+//
+//  Created by Habibur Rahman on 20/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ URLProtocol_APITesting/URLProtocol_APITesting/URLProtocol_APITestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLProtocol_APITestingApp.swift
+//  URLProtocol_APITesting
+//
+//  Created by Habibur Rahman on 20/2/25.
+//
+
+import SwiftUI
+
+@main
+struct URLProtocol_APITestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
